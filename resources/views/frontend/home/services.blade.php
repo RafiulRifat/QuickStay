a746d8959ad46f6708fa0a67b7b4533e7f835f76
RafiulRifat <rafiulsiddique301@gmail.com>
2025-02-04 19:30:00 +0600

    feat(frontend): setup room & service pages(part2)

---
+++ resources/views/frontend/home/services.blade.php
@@ -0,0 +1,63 @@
+<div class="services-area-three pt-100 pb-70 section-bg">
+            <div class="container">
+                <div class="section-title text-center">
+                    <span class="sp-color">Our Services</span>
+                    <h2>Our Services on the Global Market for Our Client's Reservation</h2>
+                </div>
+
+                <div class="row pt-45">
+                    <div class="col-lg-6 col-md-6">
+                        <div class="service-item-two">
+                            <i class="flaticon-hotel"></i>
+                            <div class="content">
+                                <h3><a href="service-details.html">Hotel Room Reservation into the Desire Places</a></h3>
+                                <p>
+                                Easily book a hotel room at your preferred destination with convenience and comfort. Enjoy a seamless reservation process and exceptional stays tailored to your needs. Experience a hassle-free and satisfying booking journey with us.
+                                </p>
+                                <a href="service-details.html" class="read-btn">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6">
+                        <div class="service-item-two">
+                            <i class="flaticon-resort"></i>
+                            <div class="content">
+                                <h3><a href="service-details.html">Resort Reservation Into the Good and Suitable Place</a></h3>
+                                <p>
+                                Find and book the ideal resort for your getaway with ease. Whether you seek relaxation or adventure, our carefully selected locations ensure a memorable experience. Enjoy seamless reservations and a stay tailored to your comfort and preferences.
+                                </p>
+                                </p>
+                                <a href="service-details.html" class="read-btn">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6">
+                        <div class="service-item-two">
+                            <i class="flaticon-buildings"></i>
+                            <div class="content">
+                                <h3><a href="service-details.html">Weeding Hall Reservation in the Suitable and Good Place</a></h3>
+                                <p>
+                                Make your special day unforgettable by booking a wedding hall in the perfect location. Whether you desire elegance, grandeur, or intimacy, we offer stunning venues tailored to your needs. Reserve now for a seamless and memorable celebration.
+                                </p>
+                                <a href="service-details.html" class="read-btn">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-md-6">
+                        <div class="service-item-two">
+                            <i class="flaticon-businessmen"></i>
+                            <div class="content">
+                                <h3><a href="service-details.html">Conference Room Reservation in the Suitable and Good Place</a></h3>
+                                <p>
+                                Book a fully-equipped conference room in a convenient and professional setting. Perfect for meetings, seminars, and corporate events, our venues offer modern amenities and a comfortable atmosphere. Reserve now for a seamless business experience.
+                                </p>
+                                <a href="service-details.html" class="read-btn">Read More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
