d277e975409a9b6f9f30c6f0df38e9ee0942f2bb
Marium2022 <prity.cse.20210204098@aust.edu>
2025-02-28 03:45:06 +0600

    Show Team In Frontend

--- resources/views/frontend/home/team.blade.php
+++ resources/views/frontend/home/team.blade.php
@@ -1,105 +1,40 @@
+@php
+    $team = App\Models\Team::latest()->get();
+@endphp
 <div class="team-area-three pt-100 pb-70">
-            <div class="container">
-                <div class="section-title text-center">
-                    <span class="sp-color">TEAM</span>
-                    <h2>Let's Meet Up With Our Special Team Members</h2>
-                </div>
-                <div class="team-slider-two owl-carousel owl-theme pt-45">
-                    <div class="team-item">
-                        <a href="team.html">
-                        <img src="{{ asset('frontend/assets/img/team/team-img2.jpg') }}" alt="Images">
-                        </a>
-                        <div class="content">
-                            <h3><a href="team.html">MD Rafiul Siddique</a></h3>
-                            <span>Manager</span>
-                            <ul class="social-link">
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-facebook'></i></a>
-                                </li> 
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-twitter'></i></a>
-                                </li> 
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-instagram'></i></a>
-                                </li> 
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-pinterest-alt'></i></a>
-                                </li> 
-                            </ul>
-                        </div>
-                    </div>
+    <div class="container">
+        <div class="section-title text-center">
+            <span class="sp-color">TEAM</span>
+            <h2>Let's Meet Up With Our Special Team Members</h2>
+        </div>
+        <div class="team-slider-two owl-carousel owl-theme pt-45">
 
-                    <div class="team-item">
-                        <a href="team.html">
-                        <img src="{{ asset('frontend/assets/img/team/team-img3.jpg') }}" alt="Images">
-                        </a>
-                        <div class="content">
-                            <h3><a href="team.html">Umme Marium Prity</a></h3>
-                            <span>Manager</span>
-                            <ul class="social-link">
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-facebook'></i></a>
-                                </li> 
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-twitter'></i></a>
-                                </li> 
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-instagram'></i></a>
-                                </li> 
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-pinterest-alt'></i></a>
-                                </li> 
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="team-item">
-                        <a href="team.html">
-                        <img src="{{ asset('frontend/assets/img/team/team-img6.jpg') }}" alt="Images">
-                        </a>
-                        <div class="content">
-                            <h3><a href="team.html">Mahadi Hasan Rubel</a></h3>
-                            <span>Housekeeping</span>
-                            <ul class="social-link">
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-facebook'></i></a>
-                                </li> 
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-twitter'></i></a>
-                                </li> 
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-instagram'></i></a>
-                                </li> 
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-pinterest-alt'></i></a>
-                                </li> 
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="team-item">
-                        <a href="team.html">
-                            <img src="assets/img/team/team-img4.jpg" alt="Images">
-                        </a>
-                        <div class="content">
-                            <h3><a href="team.html">Tom Riddle</a></h3>
-                            <span>Housekeeping</span>
-                            <ul class="social-link">
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-facebook'></i></a>
-                                </li> 
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-twitter'></i></a>
-                                </li> 
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-instagram'></i></a>
-                                </li> 
-                                <li>
-                                    <a href="#" target="_blank"><i class='bx bxl-pinterest-alt'></i></a>
-                                </li> 
-                            </ul>
-                        </div>
-                    </div>
+            @foreach ($team as $item) 
+            <div class="team-item">
+                <a href="team.html">
+<img src="{{ asset($item->image) }}" alt="Images">
+                </a>
+                <div class="content">
+<h3><a href="team.html">{{ $item->name }}</a></h3>
+                    <span>{{ $item->postion }}</span>
+                    <ul class="social-link">
+                        <li>
+<a href="{{ $item->facebook }}" target="_blank"><i class='bx bxl-facebook'></i></a>
+                        </li> 
+                        <li>
+                            <a href="#" target="_blank"><i class='bx bxl-twitter'></i></a>
+                        </li> 
+                        <li>
+                            <a href="#" target="_blank"><i class='bx bxl-instagram'></i></a>
+                        </li> 
+                        <li>
+                            <a href="#" target="_blank"><i class='bx bxl-pinterest-alt'></i></a>
+                        </li> 
+                    </ul>
                 </div>
             </div>
+            @endforeach
+
         </div>
+    </div>
+</div>
