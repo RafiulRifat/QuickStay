fae5c38c10c94225d0c50a1b51750f177066019f
Marium2022 <prity.cse.20210204098@aust.edu>
2025-02-28 23:56:37 +0600

    Room Type

--- resources/views/backend/allroom/roomtype/add_roomtype.blade.php
+++ resources/views/backend/allroom/roomtype/add_roomtype.blade.php
@@ -1,12 +1,11 @@
 @extends('admin.admin_dashboard')
 @section('admin') 
-
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
 <div class="page-content">
 				<!--breadcrumb-->
 				<div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-					<div class="breadcrumb-title pe-3">Add Room Type</div>
+					<div class="breadcrumb-title pe-3">Add Room Type </div>
 					<div class="ps-3">
 						<nav aria-label="breadcrumb">
 							<ol class="breadcrumb mb-0 p-0">
@@ -23,10 +22,12 @@
 					<div class="main-body">
 						<div class="row">
 
-                        <div class="col-lg-8">
+    <div class="col-lg-8">
         <div class="card">
+
             <form id="myForm" action="{{ route('room.type.store') }}" method="post" >
                 @csrf
+
             <div class="card-body">
                 <div class="row mb-3">
                     <div class="col-sm-3">
@@ -36,8 +37,8 @@
                         <input type="text" name="name" class="form-control"  />
                     </div>
                 </div>
-                
-                 
+
+
                 <div class="row">
                     <div class="col-sm-3"></div>
                     <div class="col-sm-9 text-secondary">
@@ -46,13 +47,18 @@
                 </div>
             </div>
         </form>
+
         </div>
-            
+
+
+
     </div>
 						</div>
 					</div>
 				</div>
 			</div>
+
+
             <script type="text/javascript">
                 $(document).ready(function (){
                     $('#myForm').validate({
@@ -86,6 +92,8 @@
                 });
                 
             </script>
+
+
       <script type="text/javascript">
         $(document).ready(function(){
             $('#image').change(function(e){
@@ -97,6 +105,8 @@
             });
         });
         </script>   
-        
-        
+
+
+
+
 @endsection
